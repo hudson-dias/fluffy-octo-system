bbe7d998f5d5897fc3d46e60d8be661245569e87
Diogo <diogo.goncalves@fornecedores.sicoob.com.br>
2022-12-14 08:12:15 -0300

    feat upload

---
+++ remove_special_dots.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# create a sample dataframe with name, address, and document number
+df = pd.DataFrame({'name': ['John Smith', 'Jane Doe'],
+                   'address': ['123 Main St.', '456 Park Ave.'],
+                   'document number': ['123-45-6789', '987.65.4321']})
+
+# print the original dataframe
+print(df)
+
+# remove special characters and accents from the name and address columns
+df['name'] = df['name'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+df['address'] = df['address'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+
+# remove dots and dashes from the document number column
+df['document number'] = df['document number'].str.replace('.', '').str.replace('-', '')
+
+# print the modified dataframe
+print(df)
